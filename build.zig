d86be5c15529c35bd780c3be2d00d8eb6ff9216d
mdavisprog <mdavisprog@gmail.com>
2026-02-26 21:57:16 -0800

    Add 'raylib' external library.

--- build.zig
+++ build.zig
@@ -4,14 +4,20 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const raylib = b.dependency("raylib", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
     const exe = b.addExecutable(.{
         .name = "ultragif",
         .root_module = b.createModule(.{
             .target = target,
             .optimize = optimize,
             .root_source_file = b.path("src/main.zig"),
-        })
+        }),
     });
+    exe.linkLibrary(raylib.artifact("raylib"));
 
     b.installArtifact(exe);
 
