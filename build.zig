ffe347663f460aec978da8639351db7af39d545a
mdavisprog <mdavisprog@gmail.com>
2026-04-12 12:29:30 -0700

    Make the run command depend on the install step.
    This will force the run command to use the installed exe inside of
    the 'zig-out' directory.

--- build.zig
+++ build.zig
@@ -43,6 +43,7 @@ pub fn build(b: *std.Build) !void {
     const run_step = b.step("run", "Runs the application.");
     const run_cmd = b.addRunArtifact(exe);
     run_step.dependOn(&run_cmd.step);
+    run_cmd.step.dependOn(b.getInstallStep());
 
     if (b.args) |args| {
         run_cmd.addArgs(args);
