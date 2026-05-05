3042cb8e965caf00e7198f92b8acebdb7cb6efeb
mdavisprog <mdavisprog@gmail.com>
2026-05-05 13:12:05 -0700

    Specify what layout to use when opening popup.
    An additional function 'openFit' has been added to provide a default
    layout that tries to fit the parent container around the contents.

--- src/gui/widgets/Popup.zig
+++ src/gui/widgets/Popup.zig
@@ -1,10 +1,17 @@
 const clay = @import("clay");
 const Container = @import("../Container.zig");
+const raylib = @import("raylib");
 const std = @import("std");
 
 pub const State = enum {
     closed,
     open,
+};
+
+pub const Options = struct {
+    layout: clay.LayoutConfig = .{
+        .sizing = .fixed(200.0, 200.0),
+    },
 };
 
 pub const OnDraw = *const fn (*Container) void;
@@ -15,11 +22,22 @@ const Self = @This();
 position: clay.Vector2 = .zero,
 state: State = .closed,
 on_draw: ?OnDraw = null,
+layout: clay.LayoutConfig = .{},
 
-pub fn open(self: *Self, position: clay.Vector2, on_draw: OnDraw) void {
+pub fn openFit(self: *Self, position: clay.Vector2, on_draw: OnDraw) void {
+    self.open(position, on_draw, .{
+        .layout = .{
+            .sizing = .fit(0.0, 0.0),
+            .padding = .axes(4, 4),
+        },
+    });
+}
+
+pub fn open(self: *Self, position: clay.Vector2, on_draw: OnDraw, options: Options) void {
     self.state = .open;
     self.position = position;
     self.on_draw = on_draw;
+    self.layout = options.layout;
 }
 
 pub fn close(self: *Self) void {
@@ -28,14 +46,15 @@ pub fn close(self: *Self) void {
 
 pub fn draw(self: *Self, container: *Container) void {
     if (self.state == .closed) return;
-    if (self.on_draw == null) return;
+    const on_draw = self.on_draw orelse return;
+
+    const id: clay.ElementId = .fromLabel("Popup");
 
     clay.openElement();
     clay.configureOpenElement(.{
+        .id = id,
         .background_color = container.state.theme.colors.background,
-        .layout = .{
-            .sizing = .fixed(200.0, 200.0),
-        },
+        .layout = self.layout,
         .floating = .{
             .attach_to = .root,
             .offset = self.position,
@@ -45,5 +64,15 @@ pub fn draw(self: *Self, container: *Container) void {
             .width = .all(1),
         },
     });
+    {
+        on_draw(container);
+    }
     clay.closeElement();
+
+    const mouse_pos = raylib.getMousePosition();
+    const element = clay.getElementData(id);
+    const hovered = element.bounding_box.contains(.init(mouse_pos.x, mouse_pos.y));
+    if (!hovered and raylib.isMouseButtonPressed(.left)) {
+        self.close();
+    }
 }
